28bc1225d5d2bcfcad8aa13a0aa69588ad0ce15c
Brock Davis <Brock Davis>
2017-01-18 13:23:17 -0600

    Actually deleted folders

--- A01 - Connect4/A01 - Connect4/Program.cs
+++ A01 - Connect4/A01 - Connect4/Program.cs
@@ -17,38 +17,135 @@ namespace A01___Connect4
             this.turn = 1;
         }
 
-        public int[,] Board {
+        public int[,] Board
+        {
             get { return board; }
         }
 
-        public void MakeMove (int column)
+        public void MakeMove(int col)
         {
-            for (int i = board[column].Length-1; i >0; i--)
+            for (int row = board.GetUpperBound(0); row >= 0; row--)
             {
-                if (board[column, i]!=0)
+                //Console.WriteLine(board[i, column]);
+                if (board[row, col] == 0)
                 {
-                    board[column, i] = turn;
-                    turn == 1 ? turn++ : turn--;
+                    //Console.WriteLine(column + " " + i);
+                    board[row, col] = turn;
+                    turn = turn == 1 ? turn + 1 : turn - 1;
+                    //Console.WriteLine(turn);
                     return;
                 }
             }
         }
 
-        public String toString();
-
+        public override string ToString()
         {
             string output = "";
-            for (int i=0;i<board.Length;i++)
+            for (int i = 0; i < board.GetUpperBound(0) + 1; i++)
             {
-                for (int j=0;j<board[0].Length;j++)
+                for (int j = 0; j < board.GetUpperBound(1) + 1; j++)
                 {
-                    if 
-                    output+=
+                    if (board[i, j] == 0)
+                        output += "O";
+                    else if (board[i, j] == 1)
+                        output += "A";
+                    else if (board[i, j] == 2)
+                        output += "B";
+                }
+                output += '\n';
+            }
+            return output;
+        }
+
+        private int CheckHorizWin(int row, int col)
+        {
+            int token = board[row, col];
+            for (int i = 0; i < 4; i++)
+            {
+                if (board[row, col + i] != token)
+                    return 0;
+            }
+            return token;
+        }
+
+        private int CheckVertWin(int row, int col)
+        {
+            int token = board[row, col];
+            for (int i = 0; i < 4; i++)
+            {
+                if (board[row + i, col] != token)
+                    return 0;
+            }
+            return token;
+        }
+
+        private int CheckBackDiagWin(int row, int col)
+        {
+            int token = board[row, col];
+            for (int i = 0; i < 4; i++)
+            {
+                if (board[row + i, col + i] != token)
+                    return 0;
+            }
+            return token;
+        }
+
+        private int CheckFrontDiagWin(int row, int col)
+        {
+            int token = board[row, col];
+            for (int i = 0; i < 4; i++)
+            {
+                if (board[row - i, col + i] != token)
+                    return 0;
+            }
+            return token;
+        }
+
+        //0 if no win, 1 if player1, 2 if player 2, -1 if draw
+        public int CheckWin()
+        {
+            Boolean openSpace = false;
+            //Loop through points on the board
+            for (int i = 0; i < board.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < board.GetUpperBound(1) + 1; j++)
+                {
+                    //Checks to see if there's still another space to play
+                    if (board[i, j] == 0)
+                        openSpace = true;
+
+
+                    //Checks for win starting from top left piece
+
+                    //Checks for horiz win if on top 3 rows
+                    Boolean horiz = (j < 4);
+                    //Checks for vert win if on left 4 cols
+                    Boolean vert = (i < 3);
+
+                    if (horiz && CheckHorizWin(i, j) != 0)
+                        return CheckHorizWin(i, j);
+
+                    if (vert && CheckVertWin(i, j) != 0)
+                        return CheckVertWin(i, j);
+
+                    //Checks for diag (top left to bottom right) win if on top 3rowx4col sector
+                    if (horiz && vert && CheckBackDiagWin(i, j) != 0)
+                        return CheckBackDiagWin(i, j);
+
+                    //Checks for diag (bottom left to top right) win if on botom 3rowx4col sector
+                    if (horiz && !vert && CheckFrontDiagWin(i, j) != 0)
+                        return CheckFrontDiagWin(i, j);
+
                 }
             }
+            if (openSpace)
+                return 0;
+            else
+                return -1;
         }
 
     }
+
 
     static class Program
     {
@@ -58,6 +155,7 @@ namespace A01___Connect4
         [STAThread]
         static void Main()
         {
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
