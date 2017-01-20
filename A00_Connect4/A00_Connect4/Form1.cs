607e123f3ab1edbc121d3e61c17aff7d63b5f9d9
Brock Davis <Brock Davis>
2017-01-20 14:27:38 -0600

    Changed project names from A00 to A01

---
+++ A00_Connect4/A00_Connect4/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A00_Connect4
+{
+    public partial class Form1 : Form
+    {
+        int x;
+        int height;
+        int width;
+        float cellWidth;
+        float cellHeight;
+        Boolean tooWide;
+        float margin;
+
+        Connect4 game;
+        int gameWinner;
+
+        public Form1()
+        {
+            game = new Connect4();
+            InitializeComponent();
+            DoubleBuffered = true;
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
+            base.OnResize(e);
+            Refresh();
+        }
+
+        protected void DrawSpaces(PaintEventArgs e)
+        {
+            int[,] board = game.Board;
+
+            if (tooWide)
+            {
+                for (int i = 0; i < board.GetUpperBound(0) + 1; i++)
+                {
+                    for (int j = 0; j < board.GetUpperBound(1) + 1; j++)
+                    {
+                        if (board[i, j] == 0)
+                            e.Graphics.FillEllipse(Brushes.White, margin + cellWidth * j, cellHeight * i, cellWidth, cellHeight);
+                        else if (board[i, j] == 1)
+                            e.Graphics.FillEllipse(Brushes.Blue, margin + cellWidth * j, cellHeight * i, cellWidth, cellHeight);
+                        else if (board[i, j] == 2)
+                            e.Graphics.FillEllipse(Brushes.DarkRed, margin + cellWidth * j, cellHeight * i, cellWidth, cellHeight);
+                    }
+                }
+            }
+            else
+            {
+
+                for (int i = 0; i < board.GetUpperBound(0) + 1; i++)
+                {
+                    for (int j = 0; j < board.GetUpperBound(1) + 1; j++)
+                    {
+                        if (board[i, j] == 0)
+                            e.Graphics.FillEllipse(Brushes.White, cellWidth * j, margin + cellHeight * i, cellWidth, cellHeight);
+                        else if (board[i, j] == 1)
+                            e.Graphics.FillEllipse(Brushes.Blue, cellWidth * j, margin + cellHeight * i, cellWidth, cellHeight);
+                        else if (board[i, j] == 2)
+                            e.Graphics.FillEllipse(Brushes.DarkRed, cellWidth * j, margin + cellHeight * i, cellWidth, cellHeight);
+                    }
+                }
+
+            }
+
+
+
+        }
+
+        //Just drawing state - not changing state
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            height = e.ClipRectangle.Height;
+            width = e.ClipRectangle.Width;
+
+            tooWide = (width * (6.0 / 7) > height);
+            if (tooWide)
+            {
+                margin = (float)(Math.Abs(height * 7.0 / 6 - width) / 2);
+                cellWidth = (width - 2 * margin) / 7;
+                cellHeight = (float)(height / 6.0);
+            }
+            else
+            {
+                margin = (float)(Math.Abs(height - (width * 6.0 / 7)) / 2);
+                cellWidth = (float)(width / 7.0);
+                cellHeight = (height - 2 * margin) / 6;
+            }
+
+
+            if (gameWinner > 0)
+            {
+                e.Graphics.FillRectangle(Brushes.Gray, 0, 0, width, height);
+                Font drawFont = new Font("Ubuntu", cellHeight * 3 / 4);
+                e.Graphics.DrawString("Player " + gameWinner + " Wins", drawFont, Brushes.Black, 0, height / 4 + cellHeight);
+
+            }
+
+            else if (gameWinner < 0)
+            {
+                e.Graphics.FillRectangle(Brushes.Gray, 0, 0, width, height);
+                Font drawFont = new Font("Ubuntu", cellHeight * 3 / 4);
+                e.Graphics.DrawString("Draw", drawFont, Brushes.Black, 0, height / 4 + cellHeight);
+
+            }
+
+            else if (tooWide)
+            {
+                e.Graphics.FillRectangle(Brushes.Chartreuse, margin, 0, (float)(height * 7.0 / 6), height);
+                DrawSpaces(e);
+            }
+
+            else
+            {
+                e.Graphics.FillRectangle(Brushes.Chartreuse, 0, margin, width, (float)(width * 6.0 / 7));
+                DrawSpaces(e);
+            }
+
+
+        }
+
+        //Good for changing state
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            x = e.X;
+
+            int col;
+
+            if (gameWinner != 0)
+            {
+                gameWinner = 0;
+                game = new Connect4();
+                Refresh();
+                base.OnMouseDown(e);
+                return;
+            }
+
+            if (tooWide)
+            {
+                col = (int)Math.Floor((x - margin) / cellWidth);
+                if (col >= 0 && col < 7)
+                    game.MakeMove(col);
+            }
+            else
+            {
+                col = (int)(x / cellWidth);
+                if (col >= 0 && col < 7)
+                    game.MakeMove(col);
+            }
+
+            gameWinner = game.CheckWin();
+
+            Refresh();
+            base.OnMouseDown(e);
+        }
+
+    }
+}
---
+++ A00_Connect4/A00_Connect4/Program.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A00_Connect4
+{
+    class Connect4
+    {
+        private int[,] board;
+        private int turn;
+
+        public Connect4()
+        {
+            this.board = new int[6, 7];
+            this.turn = 1;
+        }
+
+        public int[,] Board
+        {
+            get { return board; }
+        }
+
+        public void MakeMove(int col)
+        {
+            for (int row = board.GetUpperBound(0); row >= 0; row--)
+            {
+                if (board[row, col] == 0)
+                {
+                    board[row, col] = turn;
+                    turn = turn == 1 ? turn + 1 : turn - 1;
+                    return;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            string output = "";
+            for (int i = 0; i < board.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < board.GetUpperBound(1) + 1; j++)
+                {
+                    if (board[i, j] == 0)
+                        output += "O";
+                    else if (board[i, j] == 1)
+                        output += "A";
+                    else if (board[i, j] == 2)
+                        output += "B";
+                }
+                output += '\n';
+            }
+            return output;
+        }
+
+        private int CheckHorizWin(int row, int col)
+        {
+            int token = board[row, col];
+            for (int i = 0; i < 4; i++)
+            {
+                if (board[row, col + i] != token)
+                    return 0;
+            }
+            return token;
+        }
+
+        private int CheckVertWin(int row, int col)
+        {
+            int token = board[row, col];
+            for (int i = 0; i < 4; i++)
+            {
+                if (board[row + i, col] != token)
+                    return 0;
+            }
+            return token;
+        }
+
+        private int CheckBackDiagWin(int row, int col)
+        {
+            int token = board[row, col];
+            for (int i = 0; i < 4; i++)
+            {
+                if (board[row + i, col + i] != token)
+                    return 0;
+            }
+            return token;
+        }
+
+        private int CheckFrontDiagWin(int row, int col)
+        {
+            int token = board[row, col];
+            for (int i = 0; i < 4; i++)
+            {
+                if (board[row - i, col + i] != token)
+                    return 0;
+            }
+            return token;
+        }
+
+        //0 if no win, 1 if player1, 2 if player 2, -1 if draw
+        public int CheckWin()
+        {
+            Boolean openSpace = false;
+            //Loop through points on the board
+            for (int i = 0; i < board.GetUpperBound(0) + 1; i++)
+            {
+                for (int j = 0; j < board.GetUpperBound(1) + 1; j++)
+                {
+                    //Checks to see if there's still another space to play
+                    if (board[i, j] == 0)
+                        openSpace = true;
+
+
+                    //Checks for win starting from top left piece
+
+                    //Checks for horiz win if on top 3 rows
+                    Boolean horiz = (j < 4);
+                    //Checks for vert win if on left 4 cols
+                    Boolean vert = (i < 3);
+
+                    if (horiz && CheckHorizWin(i, j) != 0)
+                        return CheckHorizWin(i, j);
+
+                    if (vert && CheckVertWin(i, j) != 0)
+                        return CheckVertWin(i, j);
+
+                    //Checks for diag (top left to bottom right) win if on top 3rowx4col sector
+                    if (horiz && vert && CheckBackDiagWin(i, j) != 0)
+                        return CheckBackDiagWin(i, j);
+
+                    //Checks for diag (bottom left to top right) win if on botom 3rowx4col sector
+                    if (horiz && !vert && CheckFrontDiagWin(i, j) != 0)
+                        return CheckFrontDiagWin(i, j);
+
+                }
+            }
+            if (openSpace)
+                return 0;
+            else
+                return -1;
+        }
+
+    }
+
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
